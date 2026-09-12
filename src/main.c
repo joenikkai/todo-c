612a1f64312ebccb7f2a9b825ddb2a13aaf5a3bb
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-09-12 11:02:10 +0300

    feat: moved the binary name to the next line and tabbed

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@
 
 int main(int argc, char **argv) {
     if (argc < 3) {
-        printf("Usage: %s \"username\" \"objective\"\n",*argv);
+        printf("Usage: \n\t%s \"username\" \"objective\"\n",*argv);
     }
     WINDOW* root = initscr();
 #ifdef DEBUG
