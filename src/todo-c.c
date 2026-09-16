ffffe7c152eab6dbad583ae6c57ce691d31dacf7
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-09-17 00:18:07 +0300

    feat: displayed the different components

--- src/todo-c.c
+++ src/todo-c.c
@@ -69,6 +69,48 @@ char* SanitizeInput(char *msg) {
     return msg;
 }
 
+void ___todo_item_component(WINDOW **w, ToDoList_t *tsk_l) {
+    int y,x;
+    getmaxyx(*w,y,x);
+
+    int ___ORIGIN = 1;
+    int display_capacity = y / MIN_WINDOW_HEIGHT; // number of components that can be displayed at a time
+    if (display_capacity == 0 ) return;
+
+    if (display_capacity > tsk_l->capacity) {
+        display_capacity = tsk_l->capacity;
+    }
+
+    WINDOW *item_win[display_capacity];
+    int n_displayed = 0;
+
+    for (int __i = 0; __i < display_capacity; __i++) {
+        int item_y___origin = ___ORIGIN + (MIN_WINDOW_HEIGHT * __i);
+        int item_x___origin = ___ORIGIN;
+        int item_width = x - 2;
+        int item_height = MIN_WINDOW_HEIGHT;
+
+        if (item_width < 1) continue;
+
+        item_win[__i] = derwin(*w,item_height,item_width,item_y___origin,item_x___origin);
+        box(item_win[__i],DEFAULT_BORDER_CHARACTER,DEFAULT_BORDER_CHARACTER);
+
+
+        mvwprintw(
+            item_win[__i],
+            ___ORIGIN + 1,
+            ___ORIGIN,
+            " %d %s %s %s",
+            tsk_l->tasks[__i].task_number,
+            tsk_l->tasks[__i].task_status,
+            tsk_l->tasks[__i].task_description,
+            ctime(&tsk_l->tasks[__i].task_timestamp)
+        );
+        wnoutrefresh(item_win[__i]);
+    }
+    doupdate();
+
+}
 
 void __Run(ToDoList_t *tsk_l) {
     int y,x;
@@ -91,16 +133,16 @@ void __Run(ToDoList_t *tsk_l) {
     WINDOW *head = subwin(tsk_l->lwin, HEAD_HEIGHT, HEAD_WIDTH, ORIGIN, ORIGIN);
     WINDOW *tail = subwin(tsk_l->lwin, TAIL_HEIGHT, TAIL_WIDTH, HEAD_HEIGHT + BODY_HEIGHT, ORIGIN);
     WINDOW *body = subwin(tsk_l->lwin, BODY_HEIGHT, BODY_WIDTH, HEAD_HEIGHT, ORIGIN);
-    
+
     touchwin(tsk_l->lwin); 
     mvwprintw(head,0, (HEAD_WIDTH / 2) - ((PROJECT_NAME_LENGTH * 2) / 3),PROJECT_NAME);
-    box(tail, 0, 0);
-    box(body, 0, 0);
+    box(tail, DEFAULT_BORDER_CHARACTER, DEFAULT_BORDER_CHARACTER);
+    box(body, DEFAULT_BORDER_CHARACTER, DEFAULT_BORDER_CHARACTER);
     wrefresh(head);
     wrefresh(tail);
     wrefresh(body);
 #ifdef DEBUG
-    memcpy(tsk_l->tasks,EXAMPLE_DEBUG_TASKS,EXAMPLE_DEBUG_TASKS_LENGTH);
+    memcpy(tsk_l->tasks,EXAMPLE_DEBUG_TASKS,EXAMPLE_DEBUG_TASKS_LENGTH * sizeof(EXAMPLE_DEBUG_TASKS[0]));
     tsk_l->n_tasks = EXAMPLE_DEBUG_TASKS_LENGTH;
 #endif // DEBUG
     // static bool is_running = true;
@@ -109,15 +151,16 @@ void __Run(ToDoList_t *tsk_l) {
         if (tsk_l->n_tasks == 0) {
             mvwprintw(body, ((BODY_HEIGHT / 2) - 1), (BODY_WIDTH / 2 ) - (PROMPT_EMPTY__ADD_A_TASK_TODO_LENGTH / 2),PROMPT_EMPTY__ADD_A_TASK_TODO);
             mvwprintw(body, (BODY_HEIGHT / 2), (BODY_WIDTH / 2 ) - 1,"+");
+        } else {
+            ___todo_item_component(&body,tsk_l);
         }
-        strncpy((char *)INPUT_BUFFER,INPUT_TASK_PROMPT,INPUT_BUFFER_SIZE);
-
+        snprintf(INPUT_BUFFER, INPUT_BUFFER_SIZE, "%s",INPUT_TASK_PROMPT);
 
         mvwprintw(tail, REPL_CHARACTER_LABEL_ORIGIN_POSITION_Y, REPL_CHARACTER_LABEL_ORIGIN_POSITION_X, REPL_CHARACTER_LABEL "%s",INPUT_BUFFER);
         touchwin(tsk_l->lwin);
 
-        box(tail, 0, 0);
-        box(body, 0, 0);
+        box(tail, DEFAULT_BORDER_CHARACTER, DEFAULT_BORDER_CHARACTER);
+        box(body, DEFAULT_BORDER_CHARACTER, DEFAULT_BORDER_CHARACTER);
 
         wrefresh(body);
         wrefresh(tail);
